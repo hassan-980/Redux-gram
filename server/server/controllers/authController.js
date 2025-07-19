58a9fd67544ab4c3051d4696e11fcebc4d4e07df
Hassan <104513483+hassan-980@users.noreply.github.com>
2025-07-20 02:09:46 +0530

    update

--- server/server/controllers/authController.js
+++ server/server/controllers/authController.js
@@ -39,8 +39,10 @@ export const register = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        // secure: process.env.NODE_ENV === "production",
+        // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+         secure: true, // Only send over HTTPS
+         sameSite: "None", // "None" required for cross-origin cookies
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .json({
@@ -99,8 +101,10 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "Lax",
+        // secure: process.env.NODE_ENV === "production",
+        // sameSite: "Lax",
+         secure: true, // Only send over HTTPS
+  sameSite: "None", // "None" required for cross-origin cookies
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .json({ email: user.email, username: user.username });
