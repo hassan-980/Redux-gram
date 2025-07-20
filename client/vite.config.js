19cdd8a2537cc33b43b40a8a89180ed43cae7962
Hassan <104513483+hassan-980@users.noreply.github.com>
2025-07-20 15:14:08 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react()
     , tailwindcss(),
-  ],
+  ], 
+  build: {
+    outDir: 'dist',
+  },
   base: '/',
 })
